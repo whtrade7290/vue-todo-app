a4266ee926488c5cb39e5339270f58a0cb240e62
JeongWooHyeon <whtrade7290@gmail.com>
2022-11-29 01:13:15 +0900

    first commit

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
+</template>
+
+// scoped를 선언하면 이 컴포넌트 안에서만 적용
+<style scoped>
+/* 크기에 따라 배율이 정해짐 rem */
+h1 {
+ color: #2F3B52;
+ font-weight: 900;
+ margin: 2.5rem 0 1.5rem;
+ 
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <ul>
+      <li
+        v-for="(todoItem, index) in todoItems"
+        v-bind:key="todoItem.item"
+        class="shadow"
+      >
+        <i
+          class="checkBtn fas fa-check"
+          v-bind:key="todoItem.item"
+          v-bind:class="{ checkBtnCompleted: todoItem.completed }"
+          v-on:click="toggleComplete(todoItem, index)"
+        ></i>
+        <span v-bind:class="{ textCompleted: todoItem.completed }">
+          {{ todoItem.item }}</span
+        >
+        <!-- <button v-on:click="removeTodo"> -->
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      todoItems: []
+    };
+  },
+  // 인스턴스가 생성되자마자
+  created: function() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        console.log(localStorage.key(i));
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(
+            JSON.parse(localStorage.getItem(localStorage.key(i)))
+          );
+
+          // this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  },
+  methods: {
+    removeTodo: function(todoItem, index) {
+      console.log(todoItem, index);
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1); // 지우고 배열을 새로 배치
+    },
+    toggleComplete: function(todoItem, index) {
+      console.log(todoItem, index);
+      todoItem.completed = !todoItem.completed;
+      // 로컬 스토리지의 데이터를 갱신
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    }
+  }
+};
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+</style>
